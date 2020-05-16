74ef99e0370c7a3fd8911da1f6424a75880c2771
Gujju <Shirisha.Gujju@thomsonreuters.com>
2020-05-16 21:22:21 +0100

    Refactored the CostGenerator code for more legibility

--- src/main/java/service/impl/CostGeneratorImpl.java
+++ src/main/java/service/impl/CostGeneratorImpl.java
@@ -19,31 +19,54 @@ public class CostGeneratorImpl implements CostGenerator {
 
     private double totalCost = 0;
 
+    /**
+     * Accepts the scanned Item and calculates the total cost of the cart
+     * @param item
+     * @return total cost of the cart
+     */
     @Override
     public double addItemAndGetTotalCost(String item) {
         scannedItems.add(item);
         Item product =
             avaiableItems.stream().filter(availableItem -> availableItem.getName().equalsIgnoreCase(item)).findFirst().get();
-        totalCost = getTotalCost(product);
-        return totalCost;
+
+        OfferItem offerItem = getOfferedItem(product);
+        return getTotalCost(product, offerItem);
     }
 
-    private double getTotalCost(Item scannedItem) {
-        String scannedItemName = scannedItem.getName();
-        int group = Collections.frequency(scannedItems, scannedItemName);
-
-        OfferItem offeritem = offerItems.stream().filter(
-            offerItem -> (offerItem.getGroup() == group && offerItem.getItemName().equalsIgnoreCase(scannedItemName))).findFirst().orElse(null);
-
-        double priceToReduce = scannedItem.getPrice() * (group - 1);
+    /**
+     * calculated the price to adjust if there are any offer Items else calculates the total cost.
+     * @param scannedItem
+     * @param offeritem
+     * @return total cost of the cart
+     */
+    public double getTotalCost(Item scannedItem, OfferItem offeritem) {
         if (offeritem != null) {
-            scannedItems.removeIf(item -> item.equalsIgnoreCase(scannedItemName));
+            double priceToReduce = scannedItem.getPrice() * (offeritem.getGroup() - 1);
+            scannedItems.removeIf(item -> item.equalsIgnoreCase(scannedItem.getName()));
             totalCost = (totalCost - priceToReduce) + offeritem.getPrice();
         }
         else {
             totalCost = totalCost + scannedItem.getPrice();
         }
         return totalCost;
+    }
+
+    /**
+     * Searches for any offers that are available for the scanned Item
+     * @param scannedItem
+     * @return offerItem
+     */
+    public OfferItem getOfferedItem(Item scannedItem){
+        String scannedItemName = scannedItem.getName();
+        int group = Collections.frequency(scannedItems, scannedItemName);
+        OfferItem item = offerItems.stream()
+                                   .filter(offerItem ->
+                                               (offerItem.getGroup() == group && offerItem.getItemName().equalsIgnoreCase(scannedItemName)))
+                                   .findFirst()
+                                   .orElse(null);
+
+        return item;
     }
 
 }
