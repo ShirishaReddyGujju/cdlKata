e592b96d2d55d394575f02cc869d543aa9b6c036
Gujju <Shirisha.Gujju@thomsonreuters.com>
2020-05-17 14:47:15 +0100

    Added logic to get pricing rules input from the command line

--- src/main/java/service/AddItemPricingRules.java
+++ src/main/java/service/AddItemPricingRules.java
@@ -4,6 +4,7 @@ import model.Item;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class AddItemPricingRules {
     List<Item> items = new ArrayList<>();
@@ -13,7 +14,6 @@ public class AddItemPricingRules {
      * @return list of available items
      */
     public List<Item> getItemPricingList() {
-
         return items;
     }
 
@@ -22,12 +22,30 @@ public class AddItemPricingRules {
      * Adds the items to the pricing system
      */
     public void addItemPricingRules() {
+        String itemName;
+        double price;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please pass the new Item pricing details else type Exit");
+        System.out.println("Please Enter Item Name");
+        while(sc.hasNext()){
+            itemName = sc.next();
+            if (itemName.equalsIgnoreCase("exit")) {
+                System.out.println("available Items: "+ getItemPricingList());
+                return;
+            }
+            else {
+                System.out.println("Please Enter Price of " + itemName);
+                price = sc.nextDouble();
+                addItemPricing(itemName, price);
+                System.out.println("Please Enter Item Name");
+            }
+        }
 
     }
 
     public void addItemPricing(String itemName, double price){
-
-
+        Item item = new Item(itemName, price);
+        items.add(item);
     }
 
 
--- src/test/java/service/AddOfferItemPricingRulesTest.java
+++ src/test/java/service/AddOfferItemPricingRulesTest.java
@@ -11,21 +11,6 @@ class AddOfferItemPricingRulesTest {
     AddOfferItemPricingRules offerItemPricingRules = new AddOfferItemPricingRules();
 
     @Test
-    void getOfferItemRules() {
-        List<OfferItem> availableItemOffers = offerItemPricingRules.getOfferItemRules();
-
-        assertAll(() -> assertNotNull(availableItemOffers),
-                  () -> assertEquals(availableItemOffers.size(), 2),
-                  () -> assertEquals(availableItemOffers.get(0).getItemName(), "A"),
-                  () -> assertEquals(availableItemOffers.get(0).getGroup(), 3),
-                  () -> assertEquals(availableItemOffers.get(0).getPrice(), 130),
-                  () -> assertEquals(availableItemOffers.get(1).getItemName(), "B"),
-                  () -> assertEquals(availableItemOffers.get(1).getGroup(), 2),
-                  () -> assertEquals(availableItemOffers.get(1).getPrice(), 45)
-                 );
-    }
-
-    @Test
     void addOfferItemPricingRules(){
         offerItemPricingRules.addOfferItemPricing("A", 3, 130);
         offerItemPricingRules.addOfferItemPricing("B", 2, 45);
